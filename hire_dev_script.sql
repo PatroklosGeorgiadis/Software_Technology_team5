262ca38af610663f2997d5814f82c54a1c8e5dd7
PatroklosGeorgiadis <61284594+PatroklosGeorgiadis@users.noreply.github.com>
2022-06-27 12:04:09 +0300

    postgres database so far

---
+++ hire_dev_script.sql
@@ -0,0 +1,56 @@
+--CREATE USER getdevadmin WITH PASSWORD 'getdev';
+
+DROP TABLE IF EXISTS hire_dev.client;
+DROP TABLE IF EXISTS hire_dev.dev;
+DROP TABLE IF EXISTS hire_dev.project;
+
+CREATE TABLE hire_dev.client(
+	email varchar(50) NOT NULL,
+	username varchar(20) NOT NULL,
+	pass varchar(20) NOT NULL,
+	firstname varchar(30),
+	lastname varchar(30),
+	gender varchar(30),
+	birthday date,
+	pic bytea,
+	description text,
+	pagelink varchar(150),
+	PRIMARY KEY(email),
+	UNIQUE(username)
+);
+
+CREATE TABLE hire_dev.dev(
+	email varchar(50) NOT NULL,
+	username varchar(20) NOT NULL,
+	pass varchar(20) NOT NULL,
+	firstname varchar(30),
+	lastname varchar(30),
+	gender varchar(30),
+	pic bytea,
+	bio text,
+	PRIMARY KEY(email),
+	UNIQUE(username)
+);
+
+CREATE TABLE hire_dev.project(
+	title varchar(50) NOT NULL,
+	description text NOT NULL,
+	proj_type boolean NOT NULL,
+	offer_show boolean NOT NULL,
+	category text NOT NULL,
+	subcategory text NOT NULL,
+	payment_method text NOT NULL,
+	max_price int,
+	interv interval,
+	uptime int,
+	rec_tech text,
+	dev_username varchar(20) NOT NULL,
+	client_username varchar(20) NOT NULL,
+	PRIMARY KEY(title),
+	constraint fk_proj_dev
+     foreign key (dev_username) 
+     REFERENCES hire_dev.dev (username),
+	constraint fk_proj_client
+     foreign key (client_username) 
+     REFERENCES hire_dev.client (username)
+);
